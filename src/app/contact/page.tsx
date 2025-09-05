b2613e9f9a9a44ae9b9aee5521545e33e2d088a1
aklade marco <aklademarco2@gmail.com>
2025-09-05 15:24:07 +0100

    Add Contact page component with metadata and initial structure

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,9 @@
+import Contact from "../../components/Contact";
+
+export const metadata = {
+  title: "Contact | Aklade Bismark Marco"
+};
+
+export default function ContactPage() {
+  return <Contact />;
+}
